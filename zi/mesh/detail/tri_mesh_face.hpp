ccde38db966677edf9cd0e655969bd3f3ef181d5
Matt Balkam <balkamm@gmail.com>
2014-04-30 13:05:58 -0400

    Spacing.

--- zi/mesh/detail/tri_mesh_face.hpp
+++ zi/mesh/detail/tri_mesh_face.hpp
@@ -286,7 +286,7 @@ public:
                                 ( Index < 3 ), ::zi::detail::dummy< Index >
                                 >::type = 0 ) const
         {
-            return i_->second. template vertex< Index >();
+            return i_->second.template vertex< Index >();
         }
 
         inline uint32_t v0() const
